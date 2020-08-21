75b46cf5791e5079dab70f696388618193542006
Guion <mguionpratt@gmail.com>
2020-08-21 14:10:30 -0400

    Update global-find-and-replace.js

--- global-find-and-replace.js
+++ global-find-and-replace.js
@@ -1,19 +1,18 @@
 function main() {
-//- folderID = the top-level directory you want to run the script in. 
-//- oldText = existing string, 
+//- folderID = the top-level directory you want to run the script in. In Google Drive, this is everything after `folders/` in that folder's URL. 
+//- oldText = existing string. 
 //- newText = replacement string.
-    globalFindAndReplace("folderId", "oldText", "newText")
+    globalFindAndReplace("parentFolderId", "oldText", "newText")
 }
 //- Shouldn't need to touch the rest here.
 function globalFindAndReplace(parentFolderId, oldText, newText) {
     let parentFolder = DriveApp.getFolderById(parentFolderId);
     let folders = parentFolder.getFolders();
-    // iterate through folders in the folders fetched from parent folder
+    // iterate through all folders fetched from parent folder
     while (folders.hasNext()) {
         let folder = folders.next();
-        // get folder name
         Logger.log(folder.getName());
-        globalFindAndReplace(folder.getId(), oldText, newText); // look for folders inside
+        globalFindAndReplace(folder.getId(), oldText, newText);
     }
     let sheets = parentFolder.getFilesByType(MimeType.GOOGLE_SHEETS);
     while (sheets.hasNext()) {
